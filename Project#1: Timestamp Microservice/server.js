dda19cc6eee0060e5c3cc0945aef55fd983c1a5e
Austėja Rušėnaitė <79420654+rusenaite@users.noreply.github.com>
2021-03-31 22:49:21 +0300

    Update server.js

--- Project#1: Timestamp Microservice/server.js
+++ Project#1: Timestamp Microservice/server.js
@@ -56,6 +56,38 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+/** -> ‘/api/timestamp/’ endpoint that reads the date to be parsed from the URL.
+    -> generate the correct strings for unix: and utc:. */
+
+app.get("/api/timestamp/", (req, res) => {
+  res.json({
+    unix: Date.now(),
+    utc: Date()
+  });
+});
+
+app.get("/api/timestamp/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
+  if (/\d{5,}/.test(dateString)) {
+    const resDate = parseInt(dateString);
+    res.json({
+      unix: resDate,
+      utc: new Date(resDate).toUTCString()
+    });
+  } else {
+    let dateObject = new Date(dateString);
+    if (dateObject.toString() === "Invalid Date") {
+      res.json({
+        error: "Invalid Date"
+      });
+    } else {
+      res.json({
+        unix: dateObject.valueOf(),
+        utc: dateObject.toUTCString()
+      });
+    }
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
